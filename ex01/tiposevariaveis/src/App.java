0c84efff78c4189738574521e778fd6a212225bc
MBARUDI <mbarudi@hotmail.com>
2025-03-19 15:35:16 -0300

    altera tipos de variáveis e adiciona cálculo no método main

--- ex01/tiposevariaveis/src/App.java
+++ ex01/tiposevariaveis/src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         double a = 10.5;
-        double b = 20.5;
+        short b = 5;
+        int c = 7;
+        double d = a + b + c;
+        System.out.println("The result is: " + d);
     }
 }
